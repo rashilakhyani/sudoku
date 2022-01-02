1b7d539f721c52f6ccf68e6481ac7fdee45e510e
rashilakhyani <81956983+rashilakhyani@users.noreply.github.com>
2022-01-03 00:57:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { generateSudoku, checkSolution, shareUrl } from "./lib/sudoku";
+import produce from "immer";
+import SudokuBoard from "./components/SudokuBoard";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = produce({}, () => ({
+      sudoku: generateSudoku()
+    }));
+  }
+
+  handleChange = e => {
+    this.setState(
+      produce(state => {
+        state.sudoku.rows[e.row].cols[e.col].value = e.value;
+        if (!state.sudoku.solvedTime) {
+          const solved = checkSolution(state.sudoku);
+          if (solved) {
+            state.sudoku.solveTime = new Date();
+            state.sudoku.shareUrl = shareUrl(state.sudoku);
+          }
+        }
+      })
+    );
+  };
+
+  solveSudoku = e => {
+    this.setState(
+      produce(state => {
+        state.sudoku.rows.forEach(row =>
+          row.cols.forEach(col => {
+            col.value = state.sudoku.solution[col.row * 9 + col.col];
+          })
+        );
+      })
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>SUDOKU</h1>
+        </header>
+        <SudokuBoard sudoku={this.state.sudoku} onChange={this.handleChange} />
+
+        <button className="button" onClick={this.solveSudoku}>Solve it!</button>
+      </div>
+    );
+  }
+}
+
+export default App;
