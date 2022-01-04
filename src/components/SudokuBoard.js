04aad69cae31de0dff99de39a165d359c5ab900d
rashilakhyani <81956983+rashilakhyani@users.noreply.github.com>
2022-01-05 00:17:43 +0530

    Add files via upload

---
+++ src/components/SudokuBoard.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import SudokuField from "./SudokuField";
+import Timer from "./Timer";
+import Result from "./Result";
+
+export default class SudokuBoard extends Component {
+  render() {
+    const { sudoku, onChange } = this.props;
+
+    return (
+      <div>
+        {!sudoku.solveTime && <Timer start={sudoku.startTime} />}
+        {sudoku.solveTime && <Result sudoku={sudoku} />}
+        {sudoku.rows.map(row => (
+          <div className="row" key={row.index}>
+            {row.cols.map(field => (
+              <SudokuField field={field} key={field.col} onChange={onChange} />
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
